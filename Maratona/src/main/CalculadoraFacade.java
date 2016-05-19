67b47583280994bb2d8f62649b579e71f8edef66
Sucupira <luan.sucupira11@gmail.com>
2016-05-19 13:28:25 -0300

    Novo Projeto

---
+++ Maratona/src/main/CalculadoraFacade.java
@@ -0,0 +1,34 @@
+package main;
+
+public class CalculadoraFacade {
+
+	Calculadora calculadora = new Calculadora();
+	
+	public double somar(double valor1, double valor2) {
+		return 0;
+	}
+
+	public double multiplicar(double valor1, double valor2) {
+		return 0;
+	}
+
+	public double dividir(double valor1, double valor2) {
+		return 0;
+	}
+
+	public double subtrair(double valor1, double valor2) {
+		return 0;
+	}
+
+	public boolean ePar(int valor) {
+		return false;
+	}
+
+	public double potencia(double base, int potencia) {
+		return 0;
+	}
+
+	public long fatorial(int valor) {
+		return 0;
+	}
+}
